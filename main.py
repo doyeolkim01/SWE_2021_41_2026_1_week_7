237e7fc966d8d05e0729e9305fb557791542ada5
doyeolkim01 <kdy123123@g.skku.edu>
2026-04-15 19:37:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,3 @@ def twoSum(nums: List[int], target:int) -> List:
             return [num_map[complement], i]
         
         num_map[num] = i
-
-if __name__ == "__main__":
-    print(twoSum([2,7,11,15], 9))  # [0,1]
-    print(twoSum([3,2,4], 6))      # [1,2]
-    print(twoSum([3,3], 6))        # [0,1]
